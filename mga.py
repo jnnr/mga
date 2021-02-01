fc68de58308d94642a5834dc3f858bf9733c4e0d
jnnr <32454596+jnnr@users.noreply.github.com>
2021-02-01 23:44:45 +0100

    Move separate steps into functions

--- mga.py
+++ mga.py
@@ -1,38 +1,29 @@
+import pyomo.environ as po
+import copy
 
-
-
-
-def do_mga(om, slack):
-    import pyomo.environ as po
-    import copy
-
-    # get the value of the objective function
-    objective_value = om.objective.expr()
-
-    # make former objective a constraint + slack
-    function = copy.deepcopy(om.objective.expr)
-
+def add_max_cost_constraint(model, max_cost):
     def max_cost_func(m):
-        return om.objective <= (1 + slack) * objective_value
+        return model.objective <= max_cost
 
     max_cost_block = po.Block()
 
     max_cost_block.max_cost_rule = po.Constraint(rule=max_cost_func)
-    om.add_component("max_cost", max_cost_block)
 
-    # Set new objective
-    print(om.objective.expr())
+    model.add_component("max_cost", max_cost_block)
 
-    print(om.InvestmentFlow.CONVEX_INVESTFLOWS)
+    return model
+
+
+def set_new_objective(model):
 
     def obj_expr():
         # Hier will ich auf die Investflow invest variablen zugreifen, nach bestimmten sorten filtern und
         # die dann maximieren
         expr = 0
 
-        for i, o in om.InvestmentFlow.CONVEX_INVESTFLOWS:
+        for i, o in model.InvestmentFlow.CONVEX_INVESTFLOWS:
             expr += (
-                om.InvestmentFlow.invest[i, o]
+                model.InvestmentFlow.invest[i, o]
             )
             
             print(type(i))
@@ -42,17 +33,32 @@ def do_mga(om, slack):
 
         return expr
 
-    om.objective = po.Objective(sense=po.maximize, expr=obj_expr())
+    model.objective = po.Objective(sense=po.maximize, expr=obj_expr())
+
+    return model
+
+
+def do_mga(om, slack):
+
+    # get the value of the objective function
+    objective_value = om.objective.expr()
+
+    # make former objective a constraint + slack
+    function = copy.deepcopy(om.objective.expr)
+
+    max_cost = (1 + slack) * objective_value
+
+    add_max_cost_constraint(om, max_cost)
+
+    set_new_objective(om)
 
     om.write('/home/jann/Desktop/file.lp', io_options={"symbolic_solver_labels": True})
 
     om.solve(solver="cbc", solve_kwargs={"tee": True})
 
     print(om.objective.expr)
-    print(om.objective.expr())
-    om._add_objective()
+
     print(function() * (1+slack))
-    print(om.objective)
-    print(om.objective())
+    print(objective_value * (1 + slack))
 
     import sys; sys.exit()
